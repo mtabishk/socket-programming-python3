8c55fafde77e2aa2e114d70179d9f3c3f4418a6d
root <root@localhost.localdomain>
2021-01-03 22:36:58 +0530

    updated udp chat program

--- chat_udp/udp_client.py
+++ chat_udp/udp_client.py
@@ -3,10 +3,10 @@ import subprocess as sp
 import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = "192.168.1.33"
+port = 4321
 
 def recieve():
-    host = "192.168.1.28"
-    port = 1234
     s.bind( (host,port) )
     while True:
         # recieve message from server
@@ -16,8 +16,7 @@ def recieve():
         print("Server: {0} \tmessage: {1}".format(server , rec_msg) )
 
 def send():
-    msg = "start"
-    while msg != "end":
+    while True:
         local_ip = sp.getoutput("ip -4 addr show enp0s3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
         msg = input("Me@"+local_ip+": ")
         s.sendto( msg.encode() , ("192.168.1.28",1234)  )
